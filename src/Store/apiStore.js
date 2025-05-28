4e412ed41e6bcfe1c247f167fafb0e60f5584fc9
Nitish pathania <nitishpathania.dx@gmail.com>
2025-05-28 18:42:18 +0530

    V.final Updated Env

--- src/Store/apiStore.js
+++ src/Store/apiStore.js
@@ -1,9 +1,7 @@
 import axios from 'axios';
-
 // Centralized API base URL
-export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:5000/api';
+export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL
 const token = localStorage.getItem('token')||"";
-
 // Create axios instance
 const api = axios.create({
   baseURL: API_BASE_URL,
