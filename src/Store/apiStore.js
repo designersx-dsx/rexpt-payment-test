f7e20970aec5b8dc83bc7a6b4e12566de7479886
Nitish pathania <nitishpathania.dx@gmail.com>
2025-06-03 15:31:24 +0530

    ADD DOMAIN FUNCTIONALITY

--- src/Store/apiStore.js
+++ src/Store/apiStore.js
@@ -2,13 +2,13 @@ import axios from 'axios';
 // Centralized API base URL
 export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:5000/api'
 console.log(API_BASE_URL)
-const token = localStorage.getItem('token')||"";
+const token = localStorage.getItem('token') || "";
 // Create axios instance
 const api = axios.create({
   baseURL: API_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
-     Authorization: `Bearer ${token}`,
+    Authorization: `Bearer ${token}`,
   },
 });
 
@@ -16,18 +16,18 @@ const api = axios.create({
 
 // Get all knowledge bases
 export const LoginWithEmailOTP = async (email) => {
-  const res = await api.post('/auth/LoginWithEmailOTP', {email});
+  const res = await api.post('/auth/LoginWithEmailOTP', { email });
   return res;
 };
 
-export const verifyEmailOTP = async (email,otp) => {
-  const res = await api.post('/auth/verifyEmailOTP', {email,otp});
+export const verifyEmailOTP = async (email, otp) => {
+  const res = await api.post('/auth/verifyEmailOTP', { email, otp });
   return res;
 };
 
 export const getRetellVoices = async () => {
-  const res = await axios.get('https://api.retellai.com/list-voices',{
-  headers: {
+  const res = await axios.get('https://api.retellai.com/list-voices', {
+    headers: {
       Authorization: `Bearer ${process.env.REACT_APP_API_RETELL_API}`,
     },
   });
@@ -36,73 +36,72 @@ export const getRetellVoices = async () => {
 
 export const createAgent = async (data) => {
   console.log(token)
-  const res = await api.post('/agent/createAgent',data,{
-  headers: {
+  const res = await api.post('/agent/createAgent', data, {
+    headers: {
       Authorization: `Bearer ${token}`,
     },
   });
   return res;
 };
 export const fetchDashboardDetails = async (userId) => {
-  const res = await api.get(`${API_BASE_URL}/agent/getUserAgentsDetails/${userId}`,{
-  headers: {
+  const res = await api.get(`${API_BASE_URL}/agent/getUserAgentsDetails/${userId}`, {
+    headers: {
       Authorization: `Bearer ${token}`,
     },
   });
   return res.data;
 };
 
-export const fetchAgentDetailById=async(data)=>{
-  const res=await api.post('/agent/fetchAgentDetailsById',data,{
-      headers: {
+export const fetchAgentDetailById = async (data) => {
+  const res = await api.post('/agent/fetchAgentDetailsById', data, {
+    headers: {
       Authorization: `Bearer ${token}`,
     },
   })
   return res.data;
 }
 
-export const EndWebCallUpdateAgentMinutesLeft=async(data)=>{
-  const res=await api.patch(`/agent/updateAgentMinutesLeft`,data,{
-      headers: {
+export const EndWebCallUpdateAgentMinutesLeft = async (data) => {
+  const res = await api.patch(`/agent/updateAgentMinutesLeft`, data, {
+    headers: {
       Authorization: `Bearer ${token}`,
     },
   })
   return res;
 }
-export const SendScriptToDeveloper=async(data)=>{
-  const res=await api.post(`/agent/sendScriptToEmail`,data,{
-      headers: {
+export const SendScriptToDeveloper = async (data) => {
+  const res = await api.post(`/agent/sendScriptToEmail`, data, {
+    headers: {
       Authorization: `Bearer ${token}`,
     },
   })
   return res;
 }
-export const listAgents=async()=>{
-   const res = await api.get(`${API_BASE_URL}/agent/listAgents`,{
+export const listAgents = async () => {
+  const res = await api.get(`${API_BASE_URL}/agent/listAgents`, {
 
   });
   return res.data;
 }
 
-export const updateProfilePicture=async(userId,data)=>{
-   const res = await api.patch(`${API_BASE_URL}/endusers/user/update_profile_picture/${userId}`,data,{
+export const updateProfilePicture = async (userId, data) => {
+  const res = await api.patch(`${API_BASE_URL}/endusers/user/update_profile_picture/${userId}`, data, {
     headers: { 'Content-Type': 'multipart/form-data' },
   });
   return res.data;
 }
-// export const 
-// export const createKnowledgeBase = async (formData) => {
-//   const res = await api.post('/knowledge-base', formData, {
-//     headers: { 'Content-Type': 'multipart/form-data' },
-//   });
-//   return res.data;
-// };
 export const updateAgent = async (agentId, updateData) => {
   const res = await api.put(`/agent/updateAgent/${agentId}`, updateData, {
     headers: {
       Authorization: `Bearer ${token}`,
     },
   });
+  return res.data;
+};
+export const updateAgentWidgetDomain = async (id,updateData) => {
+  console.log(id,updateData)
+
+  const res = await axios.put(`${API_BASE_URL}/agent/updateAgentWidgetDomain/${id}`, updateData);
   return res.data;
 };
 
