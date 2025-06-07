97f7ce4ac2cd8b4ab998db5b09b887efa2cc2294
Nitish pathania <nitishpathania.dx@gmail.com>
2025-06-08 03:26:35 +0530

    UPDATE ABOUT BUSINESS PAGE 08-06-2025

--- src/Component/AboutBusiness/AboutBusiness.js
+++ src/Component/AboutBusiness/AboutBusiness.js
@@ -846,7 +846,7 @@ function AboutBusiness() {
                 ></textarea>
               </div>
               <div onClick={handleSkip} className={styles.skipButton}>
-               {EditingMode?"": <button>Skip for now</button>}
+               {stepEditingMode?"": <button>Skip for now</button>}
               </div>
 
               <div className={styles.fixedBtn}>
