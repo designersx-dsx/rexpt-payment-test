523d061327cc66b4bec1138edf1205b75ded0f14
NirajDX <niraj.t@designersx.com>
2025-06-12 15:16:27 +0530

    [AP] cal ui update (AP v.1.1.3)

--- src/Component/EditProfile/EditProfile.js
+++ src/Component/EditProfile/EditProfile.js
@@ -12,28 +12,47 @@ const EditProfile = () => {
             <div className={styles.infoSection}>
                 <div className={styles.header}>
                     <h3>Personal Info</h3>
-                    <span className={styles.editText}>✎ Edit</span>
+                    <span className={styles.editText}><img src='Svg/edit-icon2.svg' className={styles.PurpolIcon} />Edit</span>
                 </div>
 
-                <div className={styles.infoItem}>
-                    <label>Name</label>
-                    <p>John Vick</p>
+                <div className={styles.Part}>
+                    <img src='Svg/line-Profile.svg' />
+                    <div className={styles.infoItem}>
+                        <label>Name</label>
+                        <p>John Vick</p>
+                    </div>
+                </div>
+                <div className={styles.Part}>
+                    <img src='Svg/line-email.svg' />
+                    <div className={styles.infoItem}>
+                        <label>Email</label>
+                        <p>Johnvick@gmail.com</p>
+                    </div>
                 </div>
 
-                <div className={styles.infoItem}>
-                    <label>Email</label>
-                    <p>Johnvick@gmail.com</p>
+                <div className={styles.Part}>
+                    <img src='Svg/line-Call.svg' />
+                    <div className={styles.infoItem}>
+                        <label>Phone Number</label>
+                        <p>+91 88XX 55X XXX</p>
+                    </div>
+                </div>
+                <div className={styles.Part}>
+                    <img src='Svg/line-address.svg' />
+                    <div className={styles.infoItem}>
+                        <label>Home address</label>
+                        <p>House Number 575-C, Sector 82 near JLPL Office Mohali, (Punjab).</p>
+                    </div>
                 </div>
 
-                <div className={styles.infoItem}>
-                    <label>Phone Number</label>
-                    <p>+91 88XX 55X XXX</p>
-                </div>
 
-                <div className={styles.infoItem}>
-                    <label>Home address</label>
-                    <p>House Number 575-C, Sector 82 near JLPL Office Mohali, (Punjab).</p>
-                </div>
+
+            </div>
+             <div type="submit">
+              <div className={styles.btnTheme}>
+                <img src="svg/svg-theme.svg" alt="" />
+                <p>Save</p>
+              </div>
             </div>
         </div>
     )
