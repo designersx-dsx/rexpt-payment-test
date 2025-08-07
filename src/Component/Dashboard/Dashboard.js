a981911d87bb482d7fcdc4ee7ae591df3b30c369
Shorya Verma <shoryaverma.dx@gmail.com>
2025-08-07 17:07:53 +0530

    new changes

--- src/Component/Dashboard/Dashboard.js
+++ src/Component/Dashboard/Dashboard.js
@@ -79,7 +79,7 @@ function Dashboard() {
   const [localAgents, setLocalAgents] = useState([]);
   const [openDropdown, setOpenDropdown] = useState(null);
   const [openOffcanvas, setOpenOffcanvas] = useState(false);
-  const [timeZone, setTimeZone] = useState("")
+  // const [timeZone, setTimeZone] = useState("")
   // Cal API modal & event states
   const [isCalModalOpen, setIsCalModalOpen] = useState(false);
   const [apiKey, setApiKey] = useState("");
