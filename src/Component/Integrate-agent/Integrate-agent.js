eec430d411542b8456f2341919afe52fade35a13
Ajayy55 <ajay001.dx@gmail.com>
2025-08-27 21:37:48 +0530

    [AS]  TEST AGENT BTN script

--- src/Component/Integrate-agent/Integrate-agent.js
+++ src/Component/Integrate-agent/Integrate-agent.js
@@ -40,7 +40,7 @@ const IntegrateAgent = () => {
   const webUrl = agentDetails?.business?.webUrl
   const agentWidgetDomain = agentDetails
   const script = agentId
-    ? `<script id="rex-widget-script" src="https://velvety-arithmetic-dda3b7.netlify.app/index.js?agentId=${agentId}"></script>`
+    ? `<script id="rex-widget-script" src="https://exquisite-kitten-03990f.netlify.app/index.js?agentId=${agentId}"></script>`
     : "";
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [newDomain, setNewDomain] = useState("");
