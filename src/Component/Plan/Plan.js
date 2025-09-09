a70dbab962812cc4a08ef53c337a77a6680cf901
Shorya Verma <shoryaverma.dx@gmail.com>
2025-09-09 09:06:57 +0530

    new changes

--- src/Component/Plan/Plan.js
+++ src/Component/Plan/Plan.js
@@ -218,7 +218,7 @@ const Planss = () => {
 
         const mapCountryToCurrency = (countryCode) => {
             const countryCurrencyMap = {
-                // IN: "inr",
+                IN: "inr",
                 US: "usd",
                 CA: "cad",
                 AU: "aud",
